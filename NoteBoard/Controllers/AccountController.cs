ccbaa6503fcbd197346391602f3d853beacff759
Philip Mørch <mrphil2105@gmail.com>
2021-03-08 15:18:17 +0100

    Pass model back to view when invalid

--- NoteBoard/Controllers/AccountController.cs
+++ NoteBoard/Controllers/AccountController.cs
@@ -44,7 +44,7 @@ namespace NoteBoard.Controllers
                 }
             }
 
-            return View();
+            return View(model);
         }
 
         public IActionResult Login(string? returnUrl = null)
