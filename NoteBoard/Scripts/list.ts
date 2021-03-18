d814e4655699e502d7a950e6d416ce329e17bfd2
Philip Mørch <mrphil2105@gmail.com>
2021-03-18 18:27:50 +0100

    Move date formatting to separate script file

---
+++ NoteBoard/Scripts/list.ts
@@ -0,0 +1,17 @@
+﻿(() => {
+    const dateTimeHeadings = document.getElementsByClassName("utc-date-time") as HTMLCollectionOf<HTMLHeadingElement>;
+    const options: Intl.DateTimeFormatOptions = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        weekday: "long",
+        hour: "numeric",
+        minute: "numeric"
+    };
+
+    for (let i = 0; i < dateTimeHeadings.length; i++) {
+        const heading = dateTimeHeadings[i];
+        const dateTimeString = heading.innerText;
+        heading.innerText = new Date(dateTimeString).toLocaleString(undefined, options);
+    }
+})();
