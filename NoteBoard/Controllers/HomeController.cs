013931fec2e98c4de811c08d1f877e9960650fec
Philip Mørch <mrphil2105@gmail.com>
2021-05-06 09:42:17 +0200

    Replace home page with redirects

--- NoteBoard/Controllers/HomeController.cs
+++ NoteBoard/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NoteBoard.Data;
 
 namespace NoteBoard.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public HomeController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("List", "Board");
+            }
+
+            return RedirectToAction("Login", "Account");
         }
     }
 }
